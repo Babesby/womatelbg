526ab439211d34ad047cb9f7a957a9675c025e6a
Mzpenelope <phillipaaidoo12345@gmail.com>
2026-09-06 00:44:09 +0000

    all

--- src/canopy/canopyData.js
+++ src/canopy/canopyData.js
@@ -21,7 +21,8 @@ const envVideo=(...keys)=>{
 
 const MODULE_VIDEO_URLS={
   '01':'https://share.synthesia.io/embeds/videos/78a7b03f-3a6b-4cf2-9fc8-20e00fb50bee',
-  '02':'https://share.synthesia.io/embeds/videos/4d2a680b-b8b6-479a-908a-429ff3046abc'
+  '02':'https://share.synthesia.io/embeds/videos/4d2a680b-b8b6-479a-908a-429ff3046abc',
+  '03':'https://share.synthesia.io/embeds/videos/03d27d73-0d28-4645-864e-596b563e3f12'
 };
 
 const videoFor=(moduleId)=>envVideo(
